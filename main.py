5be1bfb6f987582c7ea93c72e01702b3e58fd0de
Takur0 <39460271+Takur0@users.noreply.github.com>
2018-10-05 05:51:35 +0900

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pprint
 import requests
 import os
 import json
+from argparse import ArgumentParser
 
 app = Flask(__name__)
 
@@ -26,5 +27,11 @@ def webhook():
     # pprint.pprint(response.json())
 
 if __name__ == '__main__':
-    app.run()
-    post_to_slack()
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
